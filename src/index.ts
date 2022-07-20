98558fe533fddc344edff88651aa6340b66ccf52
DAIKI URATA <7nohe.urata@gmail.com>
2022-07-20 23:59:22 +0900

    chore: add default output directory

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { Plugin } from "vite";
 import chokidar, { WatchOptions } from "chokidar";
 import { Options } from "openapi-typescript-codegen";
 
-interface PluginOptions extends Options {
+interface PluginOptions extends Omit<Options, "output"> {
   input: string;
-  output: string;
-  watch?: Pick<WatchOptions, 'interval'> & { disabled?: boolean };
+  output?: string;
+  watch?: Pick<WatchOptions, "interval"> & { disabled?: boolean };
 }
 
 function vitePluginOpenapiTypescriptCodegen(options: PluginOptions): Plugin {
@@ -13,24 +13,28 @@ function vitePluginOpenapiTypescriptCodegen(options: PluginOptions): Plugin {
     ignoreInitial: true,
     interval: options.watch?.interval ?? 1000,
   });
+  const openApiOptions: Options = {
+    output: "openapi",
+    ...options,
+  };
   return {
     name: "vite-plugin-openapi-typescript-codegen",
     enforce: "pre",
     buildStart: async () => {
       const OpenAPI = await import("openapi-typescript-codegen");
-      OpenAPI.generate(options);
+      OpenAPI.generate(openApiOptions);
       if (!options.watch?.disabled) {
         console.log("Watching for changes...");
         watcher.on("change", () => {
           console.log("Change detected, regenerating...");
-          OpenAPI.generate(options);
+          OpenAPI.generate(openApiOptions);
           console.log("Done!");
         });
       }
     },
     buildEnd: () => {
       watcher.close();
-    }
+    },
   };
 }
 
