531d811ee27830126adec593b9c31880656d94cd
7nohe <7nohe.urata@gmail.com>
2022-09-06 23:03:51 +0900

    fix: lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,45 @@
-import { Plugin } from "vite";
-import chokidar, { WatchOptions } from "chokidar";
-import { Options } from "openapi-typescript-codegen";
+import type { Plugin } from 'vite'
+import type { WatchOptions } from 'chokidar'
+import chokidar from 'chokidar'
+import type { Options } from 'openapi-typescript-codegen'
 
-interface PluginOptions extends Omit<Options, "output"> {
-  input: string;
-  output?: string;
-  watch?: Pick<WatchOptions, "interval"> & { disabled?: boolean };
+interface PluginOptions extends Omit<Options, 'output'> {
+  input: string
+  output?: string
+  watch?: Pick<WatchOptions, 'interval'> & { disabled?: boolean }
 }
 
 function vitePluginOpenapiTypescriptCodegen(options: PluginOptions): Plugin {
   const watcher = chokidar.watch(options.input, {
     ignoreInitial: true,
     interval: options.watch?.interval ?? 1000,
-  });
+  })
   const openApiOptions: Options = {
-    output: "openapi",
+    output: 'openapi',
     ...options,
-  };
+  }
   return {
-    name: "vite-plugin-openapi-typescript-codegen",
-    enforce: "pre",
+    name: 'vite-plugin-openapi-typescript-codegen',
+    enforce: 'pre',
     buildStart: async () => {
-      const OpenAPI = await import("openapi-typescript-codegen");
-      OpenAPI.generate(openApiOptions);
+      const OpenAPI = await import('openapi-typescript-codegen')
+      OpenAPI.generate(openApiOptions)
       if (!options.watch?.disabled) {
-        console.log("Watching for changes...");
-        watcher.on("change", () => {
-          console.log("Change detected, regenerating...");
-          OpenAPI.generate(openApiOptions);
-          console.log("Done!");
-        });
+        // eslint-disable-next-line no-console
+        console.log('Watching for changes...')
+        watcher.on('change', () => {
+          // eslint-disable-next-line no-console
+          console.log('Change detected, regenerating...')
+          OpenAPI.generate(openApiOptions)
+          // eslint-disable-next-line no-console
+          console.log('Done!')
+        })
       }
     },
     buildEnd: () => {
-      watcher.close();
+      watcher.close()
     },
-  };
+  }
 }
 
-export default vitePluginOpenapiTypescriptCodegen;
+export default vitePluginOpenapiTypescriptCodegen
